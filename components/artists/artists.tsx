f29a3c4530d65159ef2b85d28809d39a8380ed29
BlackWolfie <luc.vandevelde@naevia.fr>
2024-01-23 15:40:25 +0100

    style

--- components/artists/artists.tsx
+++ components/artists/artists.tsx
@@ -14,25 +14,25 @@ export const Artists = ({ data, theme }: { data: ArtistsType[], theme: Themes })
 
 
   return (
-    <Section color={theme._sys.filename}>
+    <Section color={theme._sys.filename} className="!bg-transparent">
       {data.map((artistData, i) => {
         const artist = artistData.node;
 
         return (
           <Container
             key={"artsit-"+i}
-            size="large"
+            size="medium"
             width="xlarge"
-            className="grid grid-cols-1 md:grid-cols-5 gap-14 items-center justify-center">
+            className="grid grid-cols-1 gap-14 justify-center items-center md:grid-cols-5">
           
-            <div className={`row-start-2 md:col-span-3 md:row-start-1 text-center md:text-left`}>
+            <div className={`row-start-2 text-center ${ i%2===0 ? 'md:col-span-3': 'md:col-start-3' } md:col-span-3 md:row-start-1 md:text-left`}>
               {artist.name && (
                 <h3
                   data-tina-field={tinaField(artist, "name")}
-                  className={`w-full relative	mb-10 text-5xl font-extrabold tracking-normal leading-tight title-font`}
+                  className={`relative mb-10 w-full text-5xl font-extrabold tracking-normal leading-tight title-font`}
                 >
                   <span
-                    className={`bg-clip-text bg-gradient-to-r `}
+                    className={`bg-clip-text bg-gradient-to-r`}
                   >
                     {artist.name}
                   </span>
@@ -41,14 +41,14 @@ export const Artists = ({ data, theme }: { data: ArtistsType[], theme: Themes })
               {artist.children && (
                 <div
                   data-tina-field={tinaField(artist, "children")}
-                  className={`prose prose-lg mx-auto md:mx-0 mb-10 prose-dark`}
+                  className={`mx-auto mb-10 prose prose-lg md:mx-0 prose-dark`}
                 >
                   <TinaMarkdown content={artist.children} />
                 </div>
               )}
               {artist.actions && (
                 <Actions
-                  className={`justify-center md:justify-start py-2`}
+                  className={`justify-center py-2 md:justify-start`}
                   parentColor={theme._sys.filename}
                   actions={artist.actions} />
               )}
@@ -56,17 +56,18 @@ export const Artists = ({ data, theme }: { data: ArtistsType[], theme: Themes })
             {artist.img && (
             <div
               data-tina-field={tinaField(artist, "img")}
-              className={`relative row-start-1 md:col-span-2 flex justify-center`}>
+              className={`flex relative row-start-1 justify-center md:col-span-2`}>
                 <img
                   className="absolute w-full rounded-lg max-w-xs md:max-w-none h-auto blur-2xl brightness-150 contrast-[0.9] dark:brightness-150 saturate-200 opacity-50 dark:opacity-30 mix-blend-multiply dark:mix-blend-hard-light"
                   src={artist.img}
                   aria-hidden="true" />
                 <img
-                  className="relative z-10 w-full max-w-xs rounded-lg md:max-w-none h-auto"
+                  className="relative z-10 w-full max-w-xs h-auto rounded-lg md:max-w-none"
                   alt={artist.name}
                   src={artist.img} />
             </div>
             )}
+            <div className={`m-auto mt-8 w-1/2 h-1 md:col-span-6`}  style={{ backgroundColor : `${theme.theme.colorSecondary? theme.theme.colorSecondary : 'black' }`}}></div>
           </Container>
         )})}
     </Section>
